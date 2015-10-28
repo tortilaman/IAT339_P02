69cc0cda46bd8b1a7100b5ff1cecadee23268211
Nick Doering <tortilaman@gmail.com>
2015-10-28 16:22:46 -0700

    Services page updated.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,51 +1,3 @@
-/*var theMargin;
-
-function changeDetailsTab(visElem) {
-	FUTURE: Perhaps there's a way to dynamically populate this instead of an array? Not important now though...
-	var tabs = ["content-details-reviews", "content-details-desc", "content-details-spec"],
-		xScrollPos = window.pageXOffset,
-		yScrollPos = window.pageYOffset,
-		i,
-		len = tabs.length;
-	console.log(xScrollPos, yScrollPos);
-	for (i = 0; i < len; i++) {
-		document.getElementById(tabs[i]).style.display = "none";
-	}
-	document.getElementById(visElem).style.display = "block";
-	window.scrollTo(xScrollPos, yScrollPos);
-	console.log(window.pageXOffset, window.pageYOffset);
-}
-
-function calcMargin() {
-	var theHead = document.getElementsByTagName("HEADER")[0],
-		headStyle = window.getComputedStyle(theHead);
-	theMargin = headStyle.getPropertyValue('margin-top');
-	console.log("Margin Calculated: " + theMargin);
-}*/
-
-/*function toggleNav() {
-	var nav = document.getElementById("main-nav"),
-		navStyle = window.getComputedStyle(nav),
-		isMobile = navStyle.getPropertyValue('position'),
-		theHead = document.getElementsByTagName("HEADER")[0],
-		headStyle = window.getComputedStyle(theHead),
-		headMargin = headStyle.getPropertyValue('margin-top');
-	console.log("Values: " + isMobile, headMargin, theMargin);
-	if (isMobile === 'relative') {
-		console.log("We're relative");
-		if (headMargin === "0px") {
-			document.getElementsByTagName("HEADER")[0].style.marginTop = theMargin;
-			console.log("Closed Menu");
-		} else {
-			//					var pxPos = theMargin.search("px");
-			//					theMargin = theMargin.substr(pxPos - 1);
-			document.getElementsByTagName("HEADER")[0].style.marginTop = 0;
-			console.log("Opened Menu");
-			calcMargin();
-		}
-	}
-}*/
-
 function checkoutStep(currentStep) {
 	var tabs = ["#personal-btn", "#shipping-btn", "#billing-btn"],
 		details = ["#personal-details", "#shipping-details", "#billing-details"];
@@ -70,3 +22,12 @@ $(document).ready(function () {
 	$("#shipping-details").css("display", "none");
 	$("#billing-details").css("display", "none");
 });
+
+function closeLightbox() {
+	$(".lightboxContainer").fadeOut(500);
+	console.log("close");
+}
+
+function openLightbox() {
+	$(".lightboxContainer").fadeIn(500);
+}
